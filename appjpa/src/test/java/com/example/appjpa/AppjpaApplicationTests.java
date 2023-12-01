db40c09e917c1a888edbbfc59c4416c53785be29
Guillermo <113456359+fertros@users.noreply.github.com>
2023-12-01 13:51:09 +0100

    Add files via upload

--- appjpa/src/test/java/com/example/appjpa/AppjpaApplicationTests.java
+++ appjpa/src/test/java/com/example/appjpa/AppjpaApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.appjpa;
 
+import com.example.appjpa.repository.AuthorRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@WebMvcTest
 class AppjpaApplicationTests {
-
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private AuthorRepository authorRepository;
 	@Test
-	void contextLoads() {
+	void findAuthorTest() {
+		//Hacer una prueba para el find author
+		
 	}
+
 
 }
